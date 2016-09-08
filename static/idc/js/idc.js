08e4d96ae117fe5aaf31e837caa79f53b238a4a3
youzeshun <ops@youzeshun.com>
2016-09-08 14:18:44 +0800

    同上

--- static/idc/js/idc.js
+++ static/idc/js/idc.js
@@ -36,7 +36,7 @@ function InitTag(jEchartData) {
             $("#drawChart-area").append(sTag);
         }
         //bootstrap栅格系统，让bootstrap设置宽度.
-        var div_new = '<div id="' + sLine + '" class="yzs-pie col-md-2 col-sm-3 col-md-6"></div>';
+        var div_new = '<div id="' + sLine + '" class="yzs-pie col-lg-2 col-md-3 col-sm-4 col-md-6 col-xs-12"></div>';
         $("#" + iColNum).append(div_new);
         ++i;
     }
