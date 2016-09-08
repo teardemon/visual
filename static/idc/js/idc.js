1e9a0d385f96705c8c2cae2ec1b53dbc57493254
youzeshun <ops@youzeshun.com>
2016-09-08 14:24:56 +0800

    栅格系统对大屏的适应暂时使用临时方案

--- static/idc/js/idc.js
+++ static/idc/js/idc.js
@@ -36,7 +36,7 @@ function InitTag(jEchartData) {
             $("#drawChart-area").append(sTag);
         }
         //bootstrap栅格系统，让bootstrap设置宽度.
-        var div_new = '<div id="' + sLine + '" class="yzs-pie col-lg-2 col-md-3 col-sm-4 col-md-6 col-xs-12"></div>';
+        var div_new = '<div id="' + sLine + '" class=""></div>';
         $("#" + iColNum).append(div_new);
         ++i;
     }
