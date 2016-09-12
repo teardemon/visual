1e5d813199142afb885efe06c0dd603d355f5ed5
chenwujie <chenwujie@henhaoji.com>
2016-09-12 14:03:21 +0800

    修复vls.sh,restart不能启动的问题，增加status来查看当前服务是否启动

--- vls.sh
+++ vls.sh
@@ -1,20 +1,15 @@
 #/bin/dash
-'''
-Created on 2016-9-5
-@author: cwj
-用途：
-	启动关闭Django服务
+#Created on 2016-9-5
+#@author: cwj
+#用途：
+#	启动关闭Django服务
 
-'''
+
 Stop(){
-	if [ -z "$num" ];then
-		echo '需要端口号'
-		exit 0
-	fi
 
 	#第一个.*表示manage.py所在的路径
 	#第二个.*表示ip访问
-	ps aux|grep -e 'python.*manage.py *runserver'.*$1|awk '{print $2}'|xargs sudo kill
+	ps aux|grep -e 'python.*manage.py *runserver'.*$num|awk '{print $2}'|xargs sudo kill
 }
 
 Start(){
@@ -23,22 +18,33 @@ Start(){
 		exit 0
 	fi
 
-	tmp=$(netstat -anp|grep tcp|cut -c 21-42|cut -d ':' -f 2|grep $num)
+	tmp=$(netstat -anp|awk '{print $4}'|awk -F ':' '{print $2}'|grep -v '^$')
 	for var in $tmp
 	do 
-	if [ "$var"=="$num" ];then
+	if [ $var = $num ];then
 		echo '端口被占用'
 		exit 0
 	fi
 	done
-	python manage.py runserver 0.0.0.0:$num &> "${basePath}/data/log/django.log"
+	nohup python manage.py runserver 0.0.0.0:$num &> "${basePath}/data/log/django.log" &
+}
+
+Status(){
+	tmp=$(ps aux|grep -e 'python.*manage.py *runserver'|awk {'print $14'}|awk -F ':' {'print $2'}|head -n 1)
+	if [ ! $tmp ];then
+		echo '服务未启动'
+		exit 0
+	else
+		echo '服务已启动，端口号为'$tmp
+		exit 0
+	fi
 }
 
 Tip(){
 	echo "语法：bash vls.sh start|stop|restart Mode port_number
 	例如：
 	启动服务：bash vls.sh start 80
-	结束服务：bash vls.sh stop 80
+	结束服务：bash vls.sh stop
 	重启服务:bash vls.sh restart 80"
 }
 
@@ -60,8 +66,11 @@ Main(){
 			Stop
 			Start
 		;;
+		status)
+			Status
+		;;
 		*)
-			echo '没有这个动作，start|restart|stop'
+			echo '没有这个动作，start|restart|stop|status'
 			Tip
 			exit
 		;;
